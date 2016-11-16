6a2d00a1c7b7dfbae3225fe6b6237be0b1f61e28
Kayden Williams <10789755@AD.UVU.EDU>
2016-11-16 11:49:35 -0700

    review

--- Program.cs
+++ Program.cs
@@ -7,6 +7,20 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            myFunction();
+            myInt = intFunction(22);
+            string myString = stringFunction(Console.ReadLine());
+            Console.WriteLine(myString);
+        }
+        public static int myInt = 10;
+        public static void myFunction (){
+            Console.WriteLine("Run Function");
+        }
+        public static int intFunction (int _peram){
+            return _peram + myInt;
+        }
+        public static string stringFunction (string _name){
+            return "You did well " + _name;
         }
     }
 }
