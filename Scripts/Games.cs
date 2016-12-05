5d09ba3b69d18f3ea8e6f80fd16c12d2ff577367
Kayden Williams <10789755@AD.UVU.EDU>
2016-12-05 11:07:01 -0700

    messed with game.cs

---
+++ Scripts/Games.cs
@@ -0,0 +1,74 @@
+using System;
+
+public class Game
+{
+    public static bool canPlay = true;
+    public Game()
+    {
+        Health.power = 100;
+        Health.message = "You are getting stronger.";
+        Ammo.message = "You have more ammo.";
+        Cave.StartMessage = "You have enterted a cave.";
+        UnderWater.objects = new String[] { "SeaWead", "Coral", "Fish", "shark" };
+    }
+    //runs at the start of the game
+    public void Start()
+    {
+        Console.WriteLine("Please type in your name:");
+        name = Console.ReadLine();
+        Console.WriteLine("Your Player name is " + name);
+        Cave.Enter();
+        while (Game.canPlay)
+        {
+            GameTimer();
+            Play();
+        }
+        Console.WriteLine("You Died");
+        Console.WriteLine("Game Over");
+    }
+    
+
+    private void Play()
+    {
+        Console.WriteLine("Play commands: Play, End, Help");
+        //gameState = Console.ReadLine();
+        switch (GameStateMachine.currentGamestate){
+            
+            case GameStateMachine.GameStates.End:
+                Console.WriteLine("Game Over");
+                Environment.Exit(0);
+                break;
+            
+            case GameStateMachine.GameStates.Help:
+                Console.WriteLine("What do you need help for? If you can't play this game, you have issues.");
+                Play();
+            break;
+           
+            case GameStateMachine.GameStates.Play:
+            break;
+           
+            default:
+                Console.WriteLine(" is not a valid option.");
+                Play();
+            break;
+        }
+
+        Random randomNum = new Random();
+        Cave.Encounter(randomNum.Next(0, Cave.objects.Length));
+    }
+    public static void GameTimer()
+    {
+        System.Threading.Thread.Sleep(2000);
+    }
+    //game levels
+    private LevelBase Cave = new CaveLevel();
+    public static LevelBase UnderWater = new LevelBase();
+    //Game powerups
+    public PowerUpBase Health = new PowerUpBase();
+    public PowerUpBase Ammo = new PowerUpBase();
+    //game weapons
+    private WeaponBase Gun = new WeaponBase();
+    private WeaponBase Rifle = new WeaponBase();
+
+
+    public string name;
